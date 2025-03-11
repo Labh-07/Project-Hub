77fea9c31b647da514da281e170121aadd72296e
Labh-07 <patellabh130605@gmail.com>
2025-03-11 17:29:21 +0530

    Update todo.js
    Now data are stored in local storage

--- Todo app/todo.js
+++ Todo app/todo.js
@@ -1,46 +1,66 @@
 //global structure :
-let todoList=[
-    {item:'Buy Milk',dueDate:'11/3/2025',},
-    {item:'Go to college',dueDate:'11/3/2025',}
-
-];
-displayItems(); //for starting display
+// let todoList=[
+//     {Task:'Buy Milk',dueDate:'11-3-2025',},
+// ];
 
 
+let todoList=[];
+restoreTask();
+displayTodo();
 
 //function :
 
-function addTodo(){
-    let inputEelemnt=document.querySelector('#todo-input');
+function addTask(){
+    let inputElement=document.querySelector('#todo-input');
     let dateElement=document.querySelector('#todo-date');
 
-    let todoItem=inputEelemnt.value;
-    let todoDate=dateElement.value;
+    let inputTask=inputElement.value;
+    let inputDate=dateElement.value;
 
-    todoList.push({item:todoItem,dueDate:todoDate});
-   
-    inputEelemnt.value=''; //after push input empty
-    dateElement.value=''; 
-
-    displayItems();
-}
-
-function displayItems(){
-    let containerElement=document.querySelector('.todo-container');
-    
-    let newHtml ='';
-
-
-    for(let i=0 ; i<todoList.length ; i++){
-        let {item,dueDate}=todoList[i]; //destructuring
-        newHtml += `
-        <span>${item}</span>
-        <span>${dueDate}</span>
-        <button class="btn-delete" onclick="
-        todoList.splice(${i},1);
-        displayItems();">Delete</button>
-        `; //here we write the ${i} becuase we are in backtik and we need to pass this loop i value in html
+    if (inputTask === "" || inputDate === "") {
+        alert("Please enter both a task and a due date.");
+        return;
     }
-
-    containerElement.innerHTML=newHtml;
+    
+    todoList.push({Task:inputTask,dueDate:inputDate});
+    displayTodo();
+    inputElement.value='';
+    dateElement.value='';
 }
+
+function displayTodo(){
+    let displayElement=document.querySelector('.todo-list');
+    // let displayTask=displayElement.value;
+
+    let newHtml='' ;
+    for(let i=0 ; i<todoList.length ;i++){
+        let task=todoList[i].Task;
+        let date=todoList[i].dueDate;
+
+        newHtml+=`
+        <span>${task}</span>
+        <span>${date}</span>
+        <button class="todo-delete" onclick="deleteTodo(${i});">Delete</button>
+        `;
+    }
+    
+    saveTask();
+    displayElement.innerHTML=newHtml;
+}
+
+function saveTask(){
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+}
+
+function deleteTodo(index){
+    todoList.splice(index,1);
+    saveTask();
+    displayTodo();
+}
+
+function restoreTask(){
+    let savedTasks = JSON.parse(localStorage.getItem("todoList")) || [];
+    todoList = savedTasks;
+    // displayTodo(); alredy call globally
+}
+
